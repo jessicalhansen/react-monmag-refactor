576135e01958827da94a208e8ae07b06b5d74b6b
jessicalh <jessicalynnehansen@gmail.com>
2021-02-11 20:02:03 -0700

    About component refactored.

--- monument-react/src/App.js
+++ monument-react/src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div>
       <Navbar />
       <Landing />
-      {/* <Blog /> */}``
+      {/* <Blog /> */}
       <Footer />
     </div>
   );
--- monument-react/src/components/Landing.js
+++ monument-react/src/components/Landing.js
@@ -1,11 +1,13 @@
 import Header from './Header';
 import Archive from './Archive';
+import About from './About';
 
 const Landing = () => {
     return (
         <>
         <Header />
         <Archive />
+        <About />
         </>
     )
 }
